0bed4046248a63fbc0dba0740b4808ab47b120d3
Sidiney Cassiano <sidineysilva1@gmail.com>
2022-05-23 21:25:14 -0300

    Camada de Dominio criada

---
+++ Domain/DailyCasesReport.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain
+{
+    public class DailyCasesReport
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string Variant { get; set; }
+        public int NumSequences { get; set; }
+        public double PercSequences { get; set; }
+        public int NumSequencesTotal { get; set; }
+    }
+}
