61c70b3a8fddf8f25afc26d5519651e51b492c7f
Sidiney Cassiano <sidineysilva1@gmail.com>
2022-05-24 11:56:45 -0300

    Projeto de Testes adicionado

---
+++ API/Controllers/DailyCasesReportController.cs
@@ -0,0 +1,33 @@
+﻿using Application.DailyCases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class DailyCasesReportController : ControllerBase
+    {
+        public DailyCasesReportController()
+        {
+           
+        }
+        
+        [HttpGet("/")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            return Ok("Fullstack Challenge 2021 🏅 - Covid Daily Cases");
+        }
+
+        [HttpGet("/dates")]
+        [ProducesResponseType(typeof(List<AvailableDatesDTO>), StatusCodes.Status200OK)]
+        public async Task<List<AvailableDatesDTO>> AvailableDates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DailyCasesReportTest/DailyCasesReportController.cs
@@ -0,0 +1,24 @@
+﻿using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace DailyCasesReportTest
+{
+    public class DailyCasesReportControllerTest
+    {
+        [Fact]
+        public void Get_Returns_StringMessage()
+        {
+            //Arrange
+            var controller = new DailyCasesReportController();
+
+            //Act
+            var actionResult = controller.Get();
+
+            //Assert
+            var result = actionResult as OkObjectResult;
+            Assert.Equal("Fullstack Challenge 2021 🏅 - Covid Daily Cases", result.Value);
+        }
+    }
+}
