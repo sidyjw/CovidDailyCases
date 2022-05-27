64ebf73b124744b236cafe487e0d3469635d41b5
Sidiney Cassiano <sidineysilva1@gmail.com>
2022-05-26 21:32:42 -0300

    Remoção de arquivos desnecessários

--- ReactClientApp/app/src/index.tsx
+++ ReactClientApp/app/src/index.tsx
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppStorage } from './state/AppContext';
 
@@ -15,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
